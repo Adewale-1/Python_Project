65440583d6c35c84eb977ee68a51b32c53f99ee4
Adewale-Adenle <56052969+Adewale-hash@users.noreply.github.com>
2022-07-23 10:05:47 -0400

    Update Encryptor.py

--- Encryptor.py
+++ Encryptor.py
@@ -1,4 +1,5 @@
-
+from art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
              'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 
              'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
